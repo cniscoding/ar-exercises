855e55f600dae11145663f032e36d5a83c5e281f
cniscoding <asdfa.com>
2023-04-06 05:43:18 -0700

    add q6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Patrick", last_name: "theStar", hourly_rate: 60)
+@store1.employees.create(first_name: "Spongebob", last_name: "Squarepants", hourly_rate: 55)
+@store2.employees.create(first_name: "Gary", last_name: "theSnail", hourly_rate: 44)
+@store2.employees.create(first_name: "Mr.", last_name: "Crabs", hourly_rate: 99)
