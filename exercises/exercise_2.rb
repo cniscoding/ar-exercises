9246618eba440b4bb31853b0f9aaeb82daefa065
cniscoding <asdfa.com>
2023-04-06 05:03:50 -0700

    add q2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+puts @store1.name
+@store2 = Store.find_by(id:2)
+puts @store2.name
+
+@store1.name = 'new name'
+@store1.save
+puts @store1.name
