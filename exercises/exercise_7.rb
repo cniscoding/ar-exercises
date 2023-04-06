58b702ef05f5b15600bc8985104080329fbb371d
cniscoding <asdfa.com>
2023-04-06 06:04:35 -0700

    add q7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Ask the user for a store name (store it in a variable)
+print "Please enter a store name > "
+@store_name = gets.chomp
+
+showError = Store.create(name: @store_name)
+puts showError.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { greater_than: 0 }
+
+  # BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
 end
