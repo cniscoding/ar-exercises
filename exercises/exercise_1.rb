67652e1e166e5af88c8ae59318d9ac7e1c210558
cniscoding <asdfa.com>
2023-04-06 04:59:38 -0700

    add q1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# burnaby = Store.new
+# burnaby.name = "Burnaby"
+# burnaby.annual_revenue = 300000
+# burnaby.mens_apparel = true
+# burnaby.womens_apparel = true
+# puts burnaby.name
+
+store = Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+store = Store.create(name: "Richmond",annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+store = Store.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
+
+puts Store.count
